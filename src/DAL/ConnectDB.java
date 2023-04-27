a30e971f32b97e85afe0ae5af1230c5fb1caff4b
Lilly <ntlilly1006@gmail.com>
2023-04-27 12:52:17 +0700

    Upload ConnectDB
    Upload db library n class connect

--- src/DAL/ConnectDB.java
+++ src/DAL/ConnectDB.java
@@ -1,7 +1,7 @@
 package DAL;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+import java.util.Vector;
 
 
 /**
@@ -9,15 +9,66 @@ import java.sql.DriverManager;
  * @author ant1006
  */
 public class ConnectDB {
-    protected Connection connect = null;
+    private final String server = "localhost";
+    private final String port = "3306";
+    private final String database = "LIBRARY_MANAGEMENT";
+    private final String url = "jdbc:mysql://" + server + ":" + port + "/" + database;
+    private final String username = "root";
+    private final String password = "qltv@1234";
+    protected Connection conn;
     
-    public ConnectDB() {
+    public boolean getConnection() {
         try {
-            Class.forName("");
-            String url = "";
-            String user = "";
-            String password = "";
-            connect = DriverManager.getConnection(url, user, password);
-        } catch (Exception e) {}
+            conn = DriverManager.getConnection(url, username, password);
+            return true;
+        } catch (Exception e) {
+            System.out.println("ERROR in class ConnectDB");
+            return false;
+        }
+    }
+    
+    public void closeConnection() {
+        try {
+            if (conn!=null) conn.close();
+        } catch (Exception ex) {
+            System.out.println("ERROR in class ConnectDB");
+        }
+    }
+    
+    public Vector<Vector<Object>> executeQuery(String sql){
+        Vector<Vector<Object>> table = new Vector<>();
+        if (getConnection())
+            try {
+                Statement stm = conn.createStatement();
+                ResultSet rs = stm.executeQuery(sql); 
+                while (rs.next()) {
+                    Vector<Object> row = new Vector<>();
+                    ResultSetMetaData rsmd = rs.getMetaData();
+                    int size = rsmd.getColumnCount();
+                    for (int i = 1; i <= size; i++) {
+                        row.add(rs.getObject(i));
+                    }
+                    table.add(row);
+                }
+            } catch (Exception ex) {
+                System.out.println("ERROR in class ConnectDB");
+            } finally {
+                closeConnection();
+            }
+        return table;
+    }
+    
+    public int executeUpdate(String sql) {
+        int rs = -1;
+        if (getConnection())
+            try {
+                Statement stm = conn.createStatement();
+                rs = stm.executeUpdate(sql);
+            } catch (Exception ex) {
+                System.out.println("ERROR in class ConnectDB");
+            } finally {
+                closeConnection();
+            }
+        return rs;
     }
 }
